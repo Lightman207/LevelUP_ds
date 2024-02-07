4e4d6358fb30546f47ca555bae5fbc11866042e7
Lightman_207 <zhokov2007@gmail.com>
2024-02-07 09:16:30 +0700

    LevelUP v0.1.3 (Connected MongoDB)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const eventHandlers = require('./handlers/eventHandlers');
+const mongoose = require('mongoose');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 
-eventHandlers(client);
+(async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL || '', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        if (mongoose.connect) {
+            console.log('I have connected to the database!');
+        } else {
+            console.log('I cannot connect to the database right now...');
+        }
 
+        eventHandlers(client);
+    } catch (error) {
+        console.log(error);
+    }
+})();
+// node --trace-warnings ...
 
 client.login(process.env.TOKEN);
